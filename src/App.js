a81927cc0f302650ca10212a57a5fe3a04dc644c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-19 22:41:32 -0200

    Add simple comment box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import CommentBox from './components/CommentBox';
 
-export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+const data = [
+	{ id: 1, author: "Mike Tyson", text: "This is one comment" },
+	{ id: 2, author: "Jennifer Lawrence", text: "This is *another* comment" }
+];
+
+class App extends Component {
+	render() {
+		return (
+			<CommentBox data={data}/>
+		);
+	}
 }
+
+export default App;
---
+++ src/components/Comment.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import marked from 'marked';
+
+class Comment extends Component {
+    rawMarkup() {
+        const rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+
+        return { __html: rawMarkup };
+    }
+
+	render() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		);
+	}
+}
+
+export default Comment;
+
+
